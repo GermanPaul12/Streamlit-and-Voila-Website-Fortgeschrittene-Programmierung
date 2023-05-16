65856648013070c2bdcb7eea4bb6e41adde0e02b
GermanPaul12 <german.paul@outlook.de>
2023-05-16 15:07:26 +0200

    add to dos

---
+++ Streamlit/Main_Page.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+ 
+st.title("Calculator App using Streamlit")
+ 
+# creates a horizontal line
+st.write("---")
+
+# input 1
+num1 = st.number_input(label="Enter first number")
+ 
+# input 2
+num2 = st.number_input(label="Enter second number")
+
+st.write("Operation")
+ 
+operation = st.radio("Select an operation to perform:",
+                    ("Add", "Subtract", "Multiply", "Divide"))
+
+
+def calculate():
+    ans = 0
+    if operation == "Add":
+        ans = num1 + num2
+    elif operation == "Subtract":
+        ans = num1 - num2
+    elif operation == "Multiply":
+        ans = num1 * num2
+    elif operation=="Divide" and num2!=0:
+        ans = num1 / num2
+    else:
+        st.warning("Division by 0 error. Please enter a non-zero number.")
+        ans = "Not defined"
+ 
+    st.success(f"Answer = {ans}")
+    
+if st.button("Calculate result"):
+    calculate()    
