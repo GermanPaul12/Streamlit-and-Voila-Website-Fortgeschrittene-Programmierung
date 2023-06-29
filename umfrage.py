179188586e70c4768c953f43113c5d1538e9b558
GermanPaul12 <german.paul@outlook.de>
2023-06-29 23:07:24 +0200

    add better xticks umfrage

--- umfrage.py
+++ umfrage.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 class Credentials:
     api_url = "https://api.sheety.co/986330bc80cb53b1444abc4dc001bec9/answersFortgeschritteneProgrammierung/formResponses1"
-    
-response = requests.get(Credentials.api_url)
-data = response.json()
-df = pd.DataFrame([[0,0]], columns = [["Streamlit", "Voila"]])
-for i in data["formResponses1"]:
-    if i["wasFindetIhrBesser?"] == "Streamlit":
-        df.Streamlit += 1
-    elif i["wasFindetIhrBesser?"] == "Voila":
-        df.Voila += 1    
-df.to_csv("UmfrageDaten.csv", index = False)        
+def get_data():    
+    response = requests.get(Credentials.api_url)
+    data = response.json()
+    df = pd.DataFrame([[0,0]], columns = [["Streamlit", "Voila"]])
+    for i in data["formResponses1"]:
+        if i["wasFindetIhrBesser?"] == "Streamlit":
+            df.Streamlit += 1
+        elif i["wasFindetIhrBesser?"] == "Voila":
+            df.Voila += 1    
+    df.to_csv("UmfrageDaten.csv", index = False)        
